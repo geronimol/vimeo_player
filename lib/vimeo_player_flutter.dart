d767c5214430bf597b43ae211b71b86d8dd9d738
geronimol <56521222+geronimol@users.noreply.github.com>
2021-08-24 20:56:06 -0300

    title and author removed

--- lib/vimeo_player_flutter.dart
+++ lib/vimeo_player_flutter.dart
@@ -54,7 +54,7 @@ class VimeoPlayer extends StatelessWidget {
              </head>
              <body>
                 <iframe 
-                src="https://player.vimeo.com/video/$videoId?loop=0&autoplay=0" 
+                src="https://player.vimeo.com/video/$videoId?loop=0&autoplay=0&byline=0&title=0"" 
                 width="100%" height="100%" frameborder="0" allow="fullscreen" 
                 allowfullscreen></iframe>
              </body>
